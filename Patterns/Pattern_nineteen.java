be1e7dfbacfc37e79c5d8386b5154c8fed89395f
Sahil Malhotra <sahil061626@gmail.com>
2024-06-07 23:56:21 +0530

    Added Pattern 19th.

---
+++ Patterns/Pattern_nineteen.java
@@ -0,0 +1,49 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_nineteen {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        s.close();
+
+        for(int i = 0;i<num;i++){
+            for (int j = num-i; j > 0; j--) {
+                System.out.print("*");
+            }
+
+            for (int k = 0; k < i; k++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < i; k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = num-i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        for(int i = 0;i<num;i++){
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            for (int k = 0; k < num - i -1; k++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < num - i -1; k++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
